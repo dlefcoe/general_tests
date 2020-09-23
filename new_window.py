8aff269b02e90a095066c3461bc2af90c5af0aed
darren lefcoe <dlefcoe@gmail.com>
2020-09-23 18:02:32 +0100

    Add files via upload
    linting update

--- new_window.py
+++ new_window.py
@@ -16,11 +16,9 @@ frame lets both exist together.
 import tkinter as tk
 import os
 
-
 # explicit imports
 from PIL import Image, ImageTk
 from tkinter import messagebox
-
 
 
 root = tk.Tk()
@@ -33,14 +31,14 @@ def open_window():
     top.title('the second window')
     top.iconbitmap('RH_icon.ico')
 
-    llb = tk.Label(top, text='hello world').pack()
-    frontlabel = tk.Label(root,text='the main one').pack()
+    tk.Label(top, text='hello world').pack()
+    tk.Label(root,text='the main one').pack()
 
-    btn2 = tk.Button(top, text='close window', command=top.destroy()).pack()
+    tk.Button(top, text='close window', command=top.destroy).pack()
 
 
     my_image = ImageTk.PhotoImage(Image.open('images/adjusted yld curve 2020-08-12 13_17_53.png'))
-    my_label = tk.Label(top, image=my_image ).pack()
+    tk.Label(top, image=my_image ).pack()
 
 button = tk.Button(root, text='open the second window', command=open_window)
 button.pack()
