781a045fb1824080ecf0ed86d8ce7f7dd4055e63
darren lefcoe <dlefcoe@gmail.com>
2021-06-22 09:06:11 +0100

    tests

---
+++ test_literal.py
@@ -0,0 +1,13 @@
+
+
+from typing import Literal
+
+
+some_string = r"C:\Users\Owner\Dropbox\programming\python\general\linkedin-bot\credentials.json"
+
+print(some_string)
+print(type(some_string))
+
+another_string = Literal["C:\Users\Owner\Dropbox\programming\python\general\linkedin-bot\credentials.json"]
+
+print(another_string)
