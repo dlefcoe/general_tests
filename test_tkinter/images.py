87b009035c7588c1247fca7425b2c4cf5eec1769
dlefcoe <dlefcoe@gmail.com>
2025-08-22 21:36:18 +0100

    ruff format

--- test_tkinter/images.py
+++ test_tkinter/images.py
@@ -1,22 +1,22 @@
-'''
+"""
 
 example by: dlefcoe
 
 dlefcoe@gmail.com
 twitter: @dlefcoe
 
-'''
+"""
 
 import os
 import tkinter as tk
 from PIL import Image, ImageTk
 
 path_to_this_folder = os.path.dirname(__file__)
-icon_image_file = os.path.join(path_to_this_folder, 'images', 'RH_icon.ico')
+icon_image_file = os.path.join(path_to_this_folder, "images", "RH_icon.ico")
 
 
 root = tk.Tk()
-root.title('LEM Solutions - hybrids images')
+root.title("LEM Solutions - hybrids images")
 root.iconbitmap(icon_image_file)
 
 
@@ -28,10 +28,8 @@ my_label = tk.Label(image=my_img)
 my_label.pack()
 
 
-button_quit = tk.Button(root, text='Exit program', command=root.quit)
+button_quit = tk.Button(root, text="Exit program", command=root.quit)
 button_quit.pack()
-
-
 
 
 root.mainloop()
